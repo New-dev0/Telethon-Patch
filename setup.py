9e01cb2800e97efe00cbeec26c1834d79323a37b
Devesh Pal <newdev0@outlook.com>
2022-04-08 21:01:18 -0700

    Release v0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from telethonpatch import __version__, __author__
 
 
-with open("README.md", "r") as readme:
+with open("README.md", "r", encoding="utf-8") as readme:
     long_description = readme.read()
 
 
@@ -10,6 +10,8 @@ setuptools.setup(
     name="telethon-patch",
     version=__version__,
     long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="GNUV3",
     author=__author__,
     author_email="New-dev0@outlook.com",
     url="https://github.com/New-dev0/Telethon-Patch",
@@ -17,5 +19,14 @@ setuptools.setup(
     install_requires=[
         "Telethon@https://github.com/Telethon-Fork/Telethon/archive/main.zip"
     ],
-    ext_package=["scripts", "data"],
+    keywords=["telethon", "telethon-patch", "telegram", "mtproto"],
+    ext_package=["scripts", "data", "examples"],
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+]
 )
