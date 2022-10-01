795fda7ad7d87f9f9723d49e3555d52656338394
Sourcery AI <>
2022-10-01 14:09:03 +0000

    'Refactored by Sourcery'

--- telethonpatch/events.py
+++ telethonpatch/events.py
@@ -48,9 +48,7 @@ class JoinRequest(EventBuilder):
                     await self.get_input_chat(), user_id=self.user_id, approved=True
                 )
             )
-            if len(res.updates) == 1:
-                return res.updates[0]
-            return res
+            return res.updates[0] if len(res.updates) == 1 else res
 
         async def reject(self):
             """Disapprove join request."""
@@ -59,9 +57,7 @@ class JoinRequest(EventBuilder):
                     await self.get_input_chat(), user_id=self.user_id, approved=False
                 )
             )
-            if len(res.updates) == 1:
-                return res.updates[0]
-            return res
+            return res.updates[0] if len(res.updates) == 1 else res
 
         async def get_user(self):
             return await self.client.get_entity(self.user_id)
