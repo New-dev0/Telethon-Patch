7be7e557d3d359841eb39dc804aa987ca262fc4b
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2022-12-01 23:10:59 +0530

    Update __init__.py

--- telethonpatch/__init__.py
+++ telethonpatch/__init__.py
@@ -2,13 +2,16 @@
 # https://github.com/New-dev0/Telethon-Patch
 # Copyright  New-dev0
 
+
+from telethon.tl.alltlobjects import LAYER
+
 __author__ = "New-Dev0"
-__version__ = "0.2"
+__version__ = f"{LAYER}.2"
 
 
 def apply():
     from . import methods, custom, events
-    from telethon import TelegramClient as client
+    from telethon import TelegramClient
 
     # Rename long name methods..
-    client.read = client.send_read_acknowledge
+    TelegramClient.read = TelegramClient.send_read_acknowledge
