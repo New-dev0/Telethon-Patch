39b5f75e4cc5868d71c4c353cbcdaa3639370af9
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2022-12-02 14:35:13 +0530

    Update __init__.py

--- telethonpatch/__init__.py
+++ telethonpatch/__init__.py
@@ -3,10 +3,38 @@
 # Copyright  New-dev0
 
 
-#from telethon.tl.alltlobjects import LAYER
+from telethon.tl.alltlobjects import LAYER
+import sys
+import os
 
 __author__ = "New-Dev0"
-__version__ = "0.2"
+__version__ = f"{LAYER}0.2"
+
+LATEST_LAYER = 149
+LATEST_TELETHON = "https://github.com/Telethon-Fork/Telethon/archive/refs/heads/main.zip"
+
+if LAYER < LATEST_LAYER:
+    sys.stderr.write(
+        """
+=========================================
+Your telegram API LAYER ({}) is outdated.
+Consider updating it to ({}) using
+'{} -m pip install --no-cache-dir {}'
+=========================================
+""".format(LAYER, LATEST_LAYER, sys.executable, LATEST_TELETHON),
+    )
+
+
+def update_telethon():
+    os.execl(
+        sys.executable,
+        sys.executable,
+        "-m",
+        "pip",
+        "install",
+        "--no-cache-dir",
+        LATEST_TELETHON,
+    )
 
 
 def apply():
